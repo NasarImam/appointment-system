8ff18985995ff67b57aa6c4b8958cc03e8579340
NasarImam <imamnasar26@gmail.com>
2026-04-29 04:08:16 +0530

    Initial commit - Patient Management System

---
+++ src/main/java/com/example/patientManagement/PatientManagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.patientManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientManagementApplication.class, args);
+
+	}
+
+}
